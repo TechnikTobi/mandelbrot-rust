5aa02c74ea43db7b12a8a389f8d4d4644e40f156
Tobias Prisching <tobias.prisching@icloud.com>
2022-09-16 20:48:07 +0200

    Update main.rs
    Using all the newly created functionality

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
 mod mandelbrot;
 mod generator;
 mod png_writer;
+mod color_map;
+mod cli;
 
 use generator::generate_raw_data;
 use png_writer::write_png;
+use color_map::{EColorMode, map_raw_to_rgb};
+use cli::CliArgs;
 
 fn main() {
 
+	let args = CliArgs::get();
+	args.print();
+
 	let test = generate_raw_data(
-		80,
-		40,
-		0.0,
-		0.0,
-		0.4,
-		100
+		args.width,
+		args.height,
+		args.x_mid,
+		args.y_mid,
+		args.zoom,
+		args.iterations
 	);
 
+	let test_rgb = map_raw_to_rgb(
+		&test, args.iterations, EColorMode::BLUE
+	);
+
+	/*
 	for y in 0..40
 	{
 		for x in 0..80
@@ -31,7 +43,12 @@ fn main() {
 		}
 		println!("");
 	}
+	*/
 
-	write_png(10, 10, "test.png", Vec::new());
-
+	write_png(
+		args.width, 
+		args.height, 
+		args.output_file_name, 
+		&test_rgb
+	);
 }
