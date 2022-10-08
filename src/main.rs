0689e6cb3eda1118fddac800dbc53040b48201a9
Tobias Prisching <tobias.prisching@icloud.com>
2022-10-08 08:49:54 +0200

    Cleaning up main.rs
    Removed junk from testing own exif writing implementation

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,12 @@ mod exif_writer;
 mod png_writer;
 mod color_map;
 mod cli;
-mod rexif;
-mod exif;
-mod exiftag;
-mod bitreader;
 
 use generator::generate_raw_data;
 use png_writer::write_png;
 use exif_writer::write_exif_description;
 use color_map::{to_EColorMode, map_raw_to_rgb};
 use cli::CliArgs;
-
-use rexif::{add_zTXt, rexif_fn};
-use exif::{ExifDate, create_exif_vec};
-use exiftag::ExifTag;
-// use miniz_oxide::deflate::compress_to_vec;
-// use deflate::deflate_bytes_zlib;
 
 fn main() {
 
@@ -48,29 +38,9 @@ fn main() {
 		&rgb_data
 	);
 
-	/*	
 	write_exif_description(
 		&args.output_file_name,
-		/* &String::from("hJ") */ &args.to_cli_string()
+		&args.to_cli_string()
 	);
 	
-	*/
-
-	let some_vec = create_exif_vec(
-		&vec![ExifDate::new(ExifTag::ImageDescription, &args.to_cli_string())]
-	);
-
-	add_zTXt(
-		&args.output_file_name,
-		&some_vec
-	);
-
-	// let compressed_vec = deflate_bytes_zlib(&some_vec);
-
-	/*	
-	rexif_fn(
-		/* &String::from("hJ.png") */ &args.output_file_name
-	);
-	*/
-
 }
