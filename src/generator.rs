9b229924a3f76b42ffbe519658073eb9a3e3eef5
Tobias Prisching <tobias.prisching@icloud.com>
2024-07-26 12:46:24 +0200

    Parallelization!

--- src/generator.rs
+++ src/generator.rs
@@ -1,3 +1,5 @@
+use rayon::prelude::*;
+
 use crate::mandelbrot::iteration_checker;
 
 const DEFAULT_SPAN: f64 = 2.0;
@@ -16,26 +18,29 @@ generate_raw_data
 {
 
 	let x_span = DEFAULT_SPAN / zoom;
-	let ppi = x_span / (width as f64);
-	let y_span = ppi * (height as f64);
+	let ppi    = x_span       / (width  as f64);
+	let y_span = ppi          * (height as f64);
 
 	let x_start = x_mid - x_span/2.0;
-	let y_end = y_mid + y_span/2.0;
+	let y_end   = y_mid + y_span/2.0;
 
-	let mut raw_data = Vec::new();
+	// Reserve & Init memory for the raw data 
+	let mut raw_data = vec![0; width as usize * height as usize];
 
-	for y in 0..height
-	{
-		for x in 0..width
+	// Use parallel iterator over this memory to compute the iteration checker
+	raw_data.par_iter_mut().enumerate().for_each(|(i, v)| 
 		{
-			raw_data.push(iteration_checker(
-				x_start + ppi * x as f64,
-				y_end - ppi * y as f64,
-				iterations
-			));
-		}
-	}
+			let y = i / width as usize;
+			let x = i % width as usize;
 
+			*v = iteration_checker(
+				x_start + ppi * x as f64,
+				y_end   - ppi * y as f64, 
+				iterations
+			);
+		}
+	);
+	
 	return raw_data;
 
 }
